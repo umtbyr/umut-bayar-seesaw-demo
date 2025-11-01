f109518da41ba89c64b38020a2721ef0495038d0
umutbayar-turib <umut.bayar@turib.com.tr>
2025-11-01 23:38:54 +0300

    feat: add fall animation to objects and counter rotate the label of the object

--- src/ui.js
+++ src/ui.js
@@ -6,24 +6,31 @@ const nextWeightInfo = document.querySelector("#next-weight-info");
 const leftWeightInfo = document.querySelector("#left-weight-info");
 const rightWeightInfo = document.querySelector("#right-weight-info");
 const tiltAngleInfo = document.querySelector("#tilt-angle-info");
+const labels = [];
 
 export const renderObject = ({ positionX, weight }) => {
-  console.log(positionX, weight);
-
+  const size = weight * OBJECT_SIZE_FACTOR + "px";
   const object = document.createElement("div");
   object.className = "object";
   //since we make the object's position absolute it will be positioned relative to seesaw (because objects are children of the seesaw)
   object.style.left = positionX + "px";
-  object.style.width = weight * OBJECT_SIZE_FACTOR + "px";
-  object.style.height = weight * OBJECT_SIZE_FACTOR + "px";
+  object.style.width = size;
+  object.style.height = size;
   object.style.background = getRandomColor();
-  object.textContent = weight;
+  object.innerHTML = `<span class="label">${weight}</span>`;
+  /*   store the label refs in runtime memory
+  to not run DOM query for every angle change */
+  labels.push(object.querySelector(".label"));
   seesaw.appendChild(object);
 };
 
 export const setAngle = (angle) => {
   tiltAngleInfo.textContent = angle.toFixed(2);
   seesaw.style.transform = `translateX(-50%) translateY(-50%) rotate(${angle}deg)`;
+
+  labels.forEach((label) => {
+    label.style.transform = `rotate(${-angle}deg)`;
+  });
 };
 
 export const setWeightInfo = ({ leftWeight, rightWeight }) => {
